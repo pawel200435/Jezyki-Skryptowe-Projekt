b678d4bb30c7c3fccff27fdab7e083c1efd9e2a6
Papit00_ <golinski-pawel@wp.pl>
2026-06-17 18:14:03 +0200

    refractor: update files to handle new db schema
    update files to work with new db models

--- add_fake_data.py
+++ add_fake_data.py
@@ -1,74 +1,80 @@
 from datetime import datetime
-from app.models import db, Ostrzezenie, Zdjecie
+from app.models import db, Warning, Product, Image
 from flask import Flask
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
 def add_fake_data():
-    ostrzezenia_data = [
+    """
+    Seeds the database with initial fake data for testing purposes.
+    """
+
+    warnings_data = [
         {
-            "gis_id": "https://www.gov.pl/web/gis/ostrzezenie-publiczne-dotyczace-zywnosci-wysoki-poziom-akryloamidu-w-okreslonych-partiach-herbatnikow",
-            "tytul": "Ostrzeżenie publiczne dotyczące żywności: wysoki poziom akryloamidu w określonych partiach herbatników",
-            "link_zrodlowy": "https://www.gov.pl/web/gis/ostrzezenie-publiczne-dotyczace-zywnosci-wysoki-poziom-akryloamidu-w-okreslonych-partiach-herbatnikow",
-            "data_publikacji": datetime(2026, 6, 11, 0, 0, 0),
-            "zagrozenie": "W wyniku badań przeprowadzonych przez Państwową Inspekcję Sanitarną stwierdzono wysoki poziom akryloamidu w herbatnikach Petit Beurre określonych poniżej. Na podstawie oceny ryzyka Narodowego Instytutu Zdrowia Publicznego PZH – Państwowego Instytutu Badawczego należy uznać, że produkt ten stanowi zagrożenie dla zdrowia konsumentów.",
-            "produkt": "Herbatniki Petit Beurre",
-            "marka": "Apetitki",
-            "numer_partii": "10.05.2027.B - 10/05/2027, 20.05.2027.A - 20/05/2027",
-            "producent": "Eurobrand Sp. z o.o. Płochocińska 111/114, 03-044 Warszawa",
-            "zalecenia": "Nie należy spożywać partii produktu określonej w tym komunikacie.",
-            "zdjecia": [
+            "link": "https://www.gov.pl/web/gis/ostrzezenie-publiczne-dotyczace-zywnosci-wysoki-poziom-akryloamidu-w-okreslonych-partiach-herbatnikow",
+            "title": "Ostrzeżenie publiczne dotyczące żywności: wysoki poziom akryloamidu w określonych partiach herbatników",
+            "publication_date": datetime(2026, 6, 11, 0, 0, 0),
+            "danger": "W wyniku badań przeprowadzonych przez Państwową Inspekcję Sanitarną stwierdzono wysoki poziom akryloamidu w herbatnikach Petit Beurre określonych poniżej. Na podstawie oceny ryzyka Narodowego Instytutu Zdrowia Publicznego PZH – Państwowego Instytutu Badawczego należy uznać, że produkt ten stanowi zagrożenie dla zdrowia konsumentów.",
+            "brand": "Apetitki",
+            "recommendations": "Nie należy spożywać partii produktu określonej w tym komunikacie.",
+    
+            "product_name": "Herbatniki Petit Beurre",
+            "batch": "10.05.2027.B - 10/05/2027, 20.05.2027.A - 20/05/2027",
+            
+            "images": [
                 {"url": "https://www.gov.pl/photo/c677fc60-c186-4e56-a7a0-b507def87307", "alt": "Opakowanie herbatników Petit Beurre Apetitki przód opakowania"},
                 {"url": "https://www.gov.pl/photo/64c224d2-1836-45e1-96ed-b5e6d92fd20b", "alt": "Opakowanie herbatników Petit Beurre Apetitki tył opakowania"}
             ]
         },
         {
-            "gis_id": "https://www.gov.pl/web/gis/ostrzezenie-publiczne-dotyczace-zywnosci-wysoki-poziom-alkaloidow-pirolizydynowych-w-produkcie-pn-pylek-kwiatowy2",
-            "tytul": "Ostrzeżenie publiczne dotyczące żywności: wysoki poziom alkaloidów pirolizydynowych w produkcie pn. Pyłek kwiatowy",
-            "link_zrodlowy": "https://www.gov.pl/web/gis/ostrzezenie-publiczne-dotyczace-zywnosci-wysoki-poziom-alkaloidow-pirolizydynowych-w-produkcie-pn-pylek-kwiatowy2",
-            "data_publikacji": datetime(2026, 6, 8, 0, 0, 0),
-            "zagrozenie": "W badaniach Państwowej Inspekcji Sanitarnej stwierdzono przekroczenie dopuszczalnego poziomu alkaloidów pirolizydynowych w przedmiotowym produkcie. Na podstawie oceny ryzyka Narodowego Instytutu Zdrowia Publicznego PZH – Państwowego Instytutu Badawczego uznano, że spożycie produktu zanieczyszczonego alkaloidami pirolizydynowymi na wykrytym poziomie może stanowić ryzyko dla zdrowia konsumentów.",
-            "produkt": "Pyłek kwiatowy, Sądecki Bartnik, 200 g",
-            "marka": "Sądecki Bartnik",
-            "numer_partii": "20/01/2028 7/4",
-            "producent": "Gospodarstwo Pasieczne ,,Sądecki Bartnik” Sp. z o.o., Stróże 235, 33-331 Stróże.",
-            "zalecenia": "Nie należy spożywać wskazanej w komunikacie partii produktu.",
-            "zdjecia": [
+            "link": "https://www.gov.pl/web/gis/ostrzezenie-publiczne-dotyczace-zywnosci-wysoki-poziom-alkaloidow-pirolizydynowych-w-produkcie-pn-pylek-kwiatowy2",
+            "title": "Ostrzeżenie publiczne dotyczące żywności: wysoki poziom alkaloidów pirolizydynowych w produkcie pn. Pyłek kwiatowy",
+            "publication_date": datetime(2026, 6, 8, 0, 0, 0),
+            "danger": "W badaniach Państwowej Inspekcji Sanitarnej stwierdzono przekroczenie dopuszczalnego poziomu alkaloidów pirolizydynowych w przedmiotowym produkcie. Na podstawie oceny ryzyka Narodowego Instytutu Zdrowia Publicznego PZH – Państwowego Instytutu Badawczego uznano, że spożycie produktu zanieczyszczonego alkaloidami pirolizydynowymi na wykrytym poziomie może stanowić ryzyko dla zdrowia konsumentów.",
+            "brand": "Sądecki Bartnik",
+            "recommendations": "Nie należy spożywać wskazanej w komunikacie partii produktu.",
+            
+            "product_name": "Pyłek kwiatowy, Sądecki Bartnik, 200 g",
+            "batch": "20/01/2028 7/4",
+            
+            "images": [
                 {"url": "https://www.gov.pl/photo/314f41cb-94bb-4ea2-b01a-e6f9d6ab472d", "alt": "Opakowanie produktu Pyłek kwiatowy Sądecki Bartnik 200 g przód opakowania"},
                 {"url": "https://www.gov.pl/photo/5ccc047d-3049-47b9-a9e5-0357d60d6c6b", "alt": "Opakowanie produktu Pyłek kwiatowy Sądecki Bartnik tył opakowania"}
             ]
         }
     ]
 
-    for item in ostrzezenia_data:
-        #Anti-duplication protection (check the unique gis_id)
-        ostrzezenie_exist = Ostrzezenie.query.filter_by(gis_id=item["gis_id"]).first()
+    for item in warnings_data:
+        #Anti-duplication protection
+        warnings_exist = Warning.query.filter_by(link=item["link"]).first()
         
-        if not ostrzezenie_exist:
-            new_o = Ostrzezenie(
-                gis_id=item["gis_id"],
-                tytul=item["tytul"],
-                link_zrodlowy=item["link_zrodlowy"],
-                data_publikacji=item["data_publikacji"],
-                zagrozenie=item["zagrozenie"],
-                produkt=item["produkt"],
-                marka=item["marka"],
-                numer_partii=item["numer_partii"],
-                producent=item["producent"],
-                zalecenia=item["zalecenia"]
+        if not warnings_exist:
+            new_warning = Warning(
+                title=item["title"],
+                link=item["link"],
+                publication_date=item["publication_date"],
+                danger=item["danger"],
+                brand=item["brand"],
+                recommendations=item["recommendations"]
             )
-            db.session.add(new_o)
+            db.session.add(new_warning)
             db.session.commit()
 
-            #Adding photos
-            for img in item["zdjecia"]:
-                new_foto = Zdjecie(
-                    ostrzezenie_id=new_o.idO,
-                    url_zdjecia=img["url"],
-                    opis_alternatywny=img["alt"]
+            new_product = Product(
+                wID=new_warning.wID,
+                product_name=item["product_name"],
+                batch=item["batch"]
+            )
+            db.session.add(new_product)
+
+            for img in item["images"]:
+                new_image = Image(
+                    wID = new_warning.wID,
+                    img_url=img["url"],
+                    alt_desc=img["alt"]
                 )
-                db.session.add(new_foto)
+                db.session.add(new_image)
             
             db.session.commit()
 
--- app/routes/ui.py
+++ app/routes/ui.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request
-from app.models import Ostrzezenie
+from app.models import Warning, Product
 import re
 
 ui_bp = Blueprint('ui', __name__)
@@ -15,7 +15,7 @@ def index():
     Fetches the 10 most recent food warnings to display on initial application load.
     """
     # Fetch latest warnings sorted by publication date descending
-    initial_alerts = Ostrzezenie.query.order_by(Ostrzezenie.data_publikacji.desc()).limit(10).all()
+    initial_alerts = Warning.query.order_by(Warning.publication_date.desc()).limit(10).all()
 
     # Pass the initial alerts to the main index template
     return render_template('pages/index.html', alerts=initial_alerts)
@@ -28,11 +28,11 @@ def search_alerts():
     """
     search_query = request.args.get('q','').strip()
     if search_query:
-        # Filter database records where the 'produkt' column contains the search query (ilike is case-insensitive)
-        filtered_alerts = Ostrzezenie.query.filter(Ostrzezenie.produkt.ilike(f'%{search_query}%')).all()
+        # Filter database records by product_name (ilike is case-insensitive)
+        filtered_alerts = Warning.query.join(Product).filter(Product.product_name.ilike(f'%{search_query}%')).all()
     else:
         # If the search field is cleared, get the 10 most recent warnings
-        filtered_alerts = Ostrzezenie.query.order_by(Ostrzezenie.data_publikacji.desc()).limit(10).all()
+        filtered_alerts = Warning.query.order_by(Warning.publication_date.desc()).limit(10).all()
     
     return render_template('partials/search_results.html', alerts=filtered_alerts)
 
