df9a5c7d06d1a507f0c4cc646b0bf84d52119aa8
Papit00_ <golinski-pawel@wp.pl>
2026-06-19 12:18:40 +0200

    feat: run Flask in a background thread
    - Moved app.run() to a separate daemon thread
    - Enabled simultaneous support for pywebview GUI and Postman API testing

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import webview
 from app import create_app
+import threading
 # enums import
 from app.enums.WindowProperties import WindowProperties
 
@@ -9,10 +10,19 @@ app = create_app()
 
 
 if __name__ == '__main__':
+
+    #new thread for flask to work in background on port 5000
+    flash_thread = threading.Thread(
+        target=app.run,
+        kwargs={'port': 5000}
+    )
+    flash_thread.daemon = True
+    flash_thread.start()
+
     #creating desktop window
     webview.create_window(
         title=WindowProperties.NAME.value, 
-        url=app,
+        url="http://127.0.0.1:5000",
         width=WindowProperties.WIDTH.value,
         height=WindowProperties.HEIGHT.value,
         resizable=WindowProperties.RESIZABLE.value,
